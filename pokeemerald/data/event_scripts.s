75b9bf6244557b7493c3eaaaaf379498c6d037f3
Lorenzo Squadrani <lorenzo.squadrani@gmail.com>
2023-03-06 01:06:52 +0100

    checco's house

--- pokeemerald/data/event_scripts.s
+++ pokeemerald/data/event_scripts.s
@@ -1057,3 +1057,5 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/birch_speech.inc"
 
 	.include "data/maps/RiminiTown_Mare/scripts.inc"
+
+	.include "data/maps/RiminiTown_CasaChecco_1F/scripts.inc"
